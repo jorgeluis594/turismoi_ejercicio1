95cb1c2c812e52e098f226a3641ddfd5a46a2a54
Jorge Gonzalez <jorg3.594@gmail.com>
2020-12-23 15:51:58 -0500

    [observer] provando observer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 import { getApiDataByCity } from "./api-wrapper.js";
-getApiDataByCity("lima").then((data) => {
-  console.log(data);
+import observer from "./observer.js";
+
+const observerApiData = observer();
+
+observerApiData.subscribe(console.log);
+
+getApiDataByCity("pucallpa").then((data) => {
+  observerApiData.notify(data);
 });
